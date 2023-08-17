c52b02dfdcb786363800f34313f3767d90a45460
kirankumar <kirankumarjami51@gmail.com>
2023-08-17 14:13:18 +0530

    array script

---
+++ 6-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# this is called array, instead of single value it can hold multiple values.
+PERSONS=("ram","sita","lakshan")
+#inside array always refer values with index and it starts from 0.
+echo "First Person ${PERSONS[0]}"
+
+echo "Total persons ${PERSONS[@]}"
