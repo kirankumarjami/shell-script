3fd40f6f3631848fcfde0aa7ff904a81de2e2335
kirankumar <kirankumarjami51@gmail.com>
2023-08-27 23:08:47 +0530

    practise

--- 7-practise-all.sh
+++ 7-practise-all.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+DATE=$(date +%F)
+LOGS_PATH=/home/centos/shell-script/shell.logs
+SCRIPT_NAME=$0
+LOG_FILE=$LOGS_PATH/$0.$DATE.log
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y=\e[33m"
 N="\e[0m"
 
 if [ $USERID -ne 0 ];
@@ -10,5 +15,16 @@ then
     echo -e "$R Failure $N"
     exit 1
 else
-    echo "$G success $N"
+    echo -e  "$G success $N"
 fi
+
+for i in $@
+do 
+    yum list installed | grep $i >$LOG_FILE
+    if [ %? -ne 0]
+    then
+        echo "package should be install"
+        yum install $i -y >$LOG_FILE
+    else
+        echo -e "$Y package is already installed $N"
+    fi 
